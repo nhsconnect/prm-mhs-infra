cecea9ab4450dd657d4c623dcb1e2c80ca37ed2d
Oktawia Kata <oktawia.kata1@nhs.net>
2021-08-19 10:26:23 +0100

    Revert "[PRMT-2096] Defined outbound http proxy and proxy port"
    This reverts commit 4bba16e2bab516b4635c84d41b7cc7a501ea2d39.

--- terraform/mhs_outbound.tf
+++ terraform/mhs_outbound.tf
@@ -60,10 +60,6 @@ resource "aws_ecs_task_definition" "mhs_outbound_task" {
           value = var.mhs_outbound_http_proxy
         },
         {
-          name = "MHS_OUTBOUND_HTTP_PROXY_PORT"
-          value = var.mhs_outbound_http_proxy_port
-        },
-        {
           name = "MHS_RESYNC_INITIAL_DELAY"
           value = var.mhs_resync_initial_delay
         }
