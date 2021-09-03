535428cb72bc26df85aa390eb8154a198b64dfa2
Oktawia Kata <oktawia.kata1@nhs.net>
2021-09-03 10:27:07 +0100

    [PRMT-2096] Converted mhs_spine_request_max_size to string for json encoding

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -51,7 +51,7 @@ variable "mhs_synchronous_endpoint_url" {
 
 variable "mhs_spine_request_max_size" {
   description = "The maximum size of the request body (in bytes) that MHS outbound sends to Spine. This should be set minus any HTTP headers and other content in the HTTP packets sent to Spine."
-  default = 4999600 # This is 5 000 000 - 400 ie 5MB - 400 bytes, roughly the size of the rest of the HTTP packet
+  default = "4999600" # This is 5 000 000 - 400 ie 5MB - 400 bytes, roughly the size of the rest of the HTTP packet
 }
 
 variable "build_id" {
