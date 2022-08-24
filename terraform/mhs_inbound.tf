bd06264a91b169567488ec6d8310633209f906bb
Mario Giampietri <mario.giampietri1@nhs.net>
2022-08-24 11:11:27 +0100

    [PRMT-2579] set number of elastic ip to 1 for perf env only

--- terraform/mhs_inbound.tf
+++ terraform/mhs_inbound.tf
@@ -278,7 +278,7 @@ resource "aws_lb" "public_inbound_nlb" {
 }
 
 resource "aws_eip" "mhs_inbound_nlb_public_ip" {
-  count = 3
+  count = var.environment == "perf" ? 1 : 3
   tags = {
     Name = "${var.environment}-${var.cluster_name}-mhs-inbound-public-ip"
   }
