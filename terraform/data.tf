2be847cc8440da6d5a0277527a5732c63cad4a4d
Ollie Beumkes <oliver.beumkes2@nhs.net>
2025-08-18 14:22:34 +0100

    Formatting Terraform code

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,7 +1,7 @@
 # Discover the VPC and private subnets to deploy MHS in.
 data "aws_vpc" "mhs" {
   filter {
-    name = "tag:Name"
+    name   = "tag:Name"
     values = ["${var.environment}-${var.cluster_name}-mhs-vpc"]
   }
 }
@@ -51,13 +51,13 @@ data "aws_route53_zone" "environment_public_zone" {
 }
 
 locals {
-  account_id = data.aws_caller_identity.current.account_id
+  account_id  = data.aws_caller_identity.current.account_id
   ecr_address = "${local.account_id}.dkr.ecr.${var.region}.amazonaws.com" # created in prm-deductions-base-infra
 
-  mhs_vpc_cidr_block = var.mhs_vpc_cidr_block
-  mhs_vpc_id = data.aws_vpc.mhs.id
-  mhs_private_subnet_ids = data.aws_subnet_ids.mhs_private.ids
-  mhs_public_subnet_ids = sort(tolist(data.aws_subnet_ids.mhs_public.ids))
+  mhs_vpc_cidr_block                       = var.mhs_vpc_cidr_block
+  mhs_vpc_id                               = data.aws_vpc.mhs.id
+  mhs_private_subnet_ids                   = data.aws_subnet_ids.mhs_private.ids
+  mhs_public_subnet_ids                    = sort(tolist(data.aws_subnet_ids.mhs_public.ids))
   mhs_dynamodb_vpc_endpoint_prefix_list_id = data.aws_vpc_endpoint.mhs-dynamodb.prefix_list_id
-  mhs_s3_vpc_endpoint_prefix_list_id = data.aws_vpc_endpoint.mhs-s3.prefix_list_id
+  mhs_s3_vpc_endpoint_prefix_list_id       = data.aws_vpc_endpoint.mhs-s3.prefix_list_id
 }
--- terraform/dev_test_harness.tfvars
+++ terraform/dev_test_harness.tfvars
@@ -1,5 +1,5 @@
-cluster_name = "test-harness"
-cluster_suffix = "test-harness-mhs"
+cluster_name       = "test-harness"
+cluster_suffix     = "test-harness-mhs"
 inbound_queue_name = "test-harness-inbound"
 mhs_vpc_cidr_block = "10.34.128.0/17"
 recipient_ods_code = "C81116"
